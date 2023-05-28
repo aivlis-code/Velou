90e3e5bb77d16c03a0f67a8e805bfacc600eeb75
zurilorc <128915445+zurilorc@users.noreply.github.com>
2023-05-28 19:44:18 +0200

    Update and rename users.sql to velou.sql

---
+++ velou.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE Velou;
+
+USE Velou;
+
+CREATE TABLE velos (
+    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    image_url VARCHAR(255)
+);
+
+CREATE TABLE users (
+    id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
