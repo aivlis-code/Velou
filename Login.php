eb193808ad51100d285336b4be2bc3e932eff9c4
zurilorc <128915445+zurilorc@users.noreply.github.com>
2023-06-02 20:27:14 +0200

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,32 +1,108 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Vélou</title>
-    <link rel="shortcut icon" href="Pictures/LogoAppli.png" type="image/x-icon"/>
-    <link rel="stylesheet" type="text/css" href="stylevelou.css">
-</head>
-<body>
-    <!-- Nous avons inclu notre page Common pour apporter les infos et functions comme print_nav et print_footer -->
-    <?php include 'Common.php'; 
-    print_header(); 
-    print_nav();
-    ?>
-    <!-- Nous avons mis un container-->
-    <div class="container">
-        <h2>Login</h2>
+:root {
+    --primary-color: #8CB561; /* Vert */
+    --secondary-color: #F6EFE2; /* Beige */
+    --highlight-color: #000000; /* Noir */
+    --font-family: 'Roboto', sans-serif;
+    --form-spacing: 20px;
+}
 
-        <!-- Formulaire de connexion -->
-        <form action="login_reply.php" method="post">
-            <input type="email" id="email" name="email" placeholder="Email" required>
-            <input type="password" id="password" name="password" placeholder="Mot de passe" required>
-            <input type="submit" id="submit" value="Se connecter">
-        </form>
+body {
+    font-family: var(--font-family);
+    background-color: var(--secondary-color);
+    color: var(--primary-color);
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    justify-content: space-between; /* Force le pied de page au bas */
+}
 
-        <!-- Lien vers la page d'inscription -->
-        <a href="Register.php">Register</a>
-    </div>
-    <?php 
-    //Nous ajoutons la fonction print_footer
-    print_footer();?>
-</body>
-</html>
+h2, h3, h4, h5, h6, ul, p {
+    text-align: center;
+    color: var(--highlight-color);
+    margin-bottom: 24px;
+    margin-top: var(--form-spacing);
+}
+
+nav ul {
+    padding: 0;
+}
+
+nav ul li a {
+    color: var(--highlight-color);
+    text-decoration: none;
+    padding: 10px;
+    transition: all 0.3s ease;
+    box-shadow: none;
+}
+
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    flex-grow: 1;
+    gap: var(--form-spacing); /* Ajoute un espace entre les éléments du formulaire */
+}
+
+.container a {
+    display: inline-block; /* Permet de centrer les éléments du lien */
+    margin-top: var(--form-spacing);
+    text-align: center;
+}
+
+button, a {
+    background-color: var(--primary-color);
+    color: var(--highlight-color);
+    border: none;
+    padding: 10px;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    text-decoration: none;
+}
+
+button:hover, a:hover {
+    background-color: var(--highlight-color);
+    color: var(--primary-color);
+}
+
+.bike_details {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    gap: 2em;
+}
+
+.bike_details img {
+    max-width: 250px;
+    max-height: 300px;
+    height: auto;
+    flex-shrink: 0;
+    order: 1;
+}
+
+.bike_details p {
+    flex-grow: 1;
+    text-align: right;
+    order: 2;
+}
+
+form {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    gap: var(--form-spacing);
+}
+
+form input[type="submit"], form button {
+    margin-top: var(--form-spacing);
+}
+
+form label, form input[type="date"], form input[type="submit"] {
+    display: block;
+    margin-bottom: 1em;
+}
+
+footer {
+    order: 3; /* Force le pied de page au bas */
+}
